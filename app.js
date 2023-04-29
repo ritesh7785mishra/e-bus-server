56f2326023654ae7083dbf7e135cb7776c007513
unknown <ritesh7785mishra@gmail.com>
2023-04-29 13:46:28 +0530

    small changes before deploying

--- app.js
+++ app.js
@@ -10,12 +10,14 @@ const conductorRouter = require("./routers/conductorRouter");
 const userRouter = require("./routers/userRouter");
 const bodyParser = require("body-parser");
 
+const { frontendUrl } = process.env;
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: "https://e-savari.netlify.app",
+    origin: frontendUrl,
   })
 );
 app.listen(3000);
